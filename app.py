7cfa1faba97b6c771922a9bb6d62806bc7c4081b
yashgaur969 <yashgaur969@gmail.com>
2020-06-13 17:05:25 +0530

    syntax error

--- app.py
+++ app.py
@@ -26,6 +26,8 @@ api.add_resource(Item, '/item/<string:name>')
 api.add_resource(ItemList, '/items')
 api.add_resource(UserRegister, '/register')
 
-db.init_app(app)
+
 if __name__ == '__main__':
+    from db import db
+    db.init_app(app)
     app.run(port=5000, debug=True)
